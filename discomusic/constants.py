043bb7bac32ce36bc5ca07147adfe96133f60da7
Jordan Stocker <Pvthaggard@gmail.com>
2018-12-29 14:31:06 +1030

    Changed config/serverconfig teplates and added new command aliases

--- discomusic/constants.py
+++ discomusic/constants.py
@@ -3,14 +3,16 @@ import os
 CONFIG_PATH = os.path.realpath(path="./config/config.ini")
 SERVER_CONFIG_PATH = os.path.realpath(path="./config/server_config.ini")
 
-CONFIG_TEMPLATE = {"bot": {"discord_token": "https://discordapp.com/developers/applications",
-                           "admin": "[name]discord_user_id"},
-                   "google API": {"key": "https://console.cloud.google.com/apis/api/youtube.googleapis.com"}}
+CONFIG_TEMPLATE = {"discord": {"token": "https://discordapp.com/developers/applications",
+                               "admin": "[human readable name]discord_user_id"},
+                   "youtube": {"token": "https://console.cloud.google.com/apis/api/youtube.googleapis.com"}}
 
 SERVER_CONFIG_TEMPLATE = {"DEFAULT": {"prefix": '/',
-                                      "volume": "0.5",
-                                      "blacklist": ""}}
+                                      "volume": "1",
+                                      "user_blacklist": ""}}
 COMMAND_ALIASES = {"p": "pause",
                    "s": "stop",
+                   "n": "next",
                    "np": "nowplaying",
-                   "h": "help"}
+                   "h": "help",
+                   "v": "volume"}
